272cded773e5893b4d551169456f71866b37f8a8
Elgen <143238582+Elgen69@users.noreply.github.com>
2025-06-22 15:42:55 +0800

    Create hello.m

---
+++ hello.m
@@ -0,0 +1,10 @@
+// hello.m - Basic Objective-C Hello World
+
+#import <Foundation/Foundation.h>
+
+int main() {
+    @autoreleasepool {
+        NSLog(@"Hello, Objective-C!");
+    }
+    return 0;
+}
