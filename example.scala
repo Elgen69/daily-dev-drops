c23cf592442c1687b6f91d4a2f8086d13acb65d8
Elgen <143238582+Elgen69@users.noreply.github.com>
2025-06-22 15:42:12 +0800

    Create example.scala

---
+++ example.scala
@@ -0,0 +1,11 @@
+// example.scala - Hello from Scala
+
+object Example {
+  def greet(name: String): String = {
+    s"Hello, $name!"
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(greet("Elgen"))
+  }
+}
