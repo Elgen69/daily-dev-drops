933447d2c2bb10958ae0981cd2c2ab56dae9a8a9
Elgen <143238582+Elgen69@users.noreply.github.com>
2025-06-22 15:41:06 +0800

    Create calc.swift

---
+++ calc.swift
@@ -0,0 +1,12 @@
+// calc.swift - Simple calculator in Swift
+
+func add(_ a: Int, _ b: Int) -> Int {
+    return a + b
+}
+
+func multiply(_ a: Int, _ b: Int) -> Int {
+    return a * b
+}
+
+print("Sum: \(add(4, 5))")
+print("Product: \(multiply(3, 7))")
